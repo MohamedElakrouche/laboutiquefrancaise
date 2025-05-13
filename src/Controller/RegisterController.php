40fe95a2b81eddcd0a0855044a662683a83d870f
Mohamed el akrouche <elakrouche@gmail.com>
2025-05-13 13:41:30 +0200

    Integration_Login_Logout

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -4,13 +4,12 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegisterUserTypeForm;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 final class RegisterController extends AbstractController
 {
@@ -19,19 +18,12 @@ final class RegisterController extends AbstractController
     {
        $user = new User();
         $form=$this->createForm(RegisterUserTypeForm::class , $user);
-
-        
-        
-        
 $form->handleRequest($request);
-// SI le formulaire est soumis et valide
 if ($form->isSubmitted() && $form->isValid()) {
     // Tu enregistre l'utilisateur en BDD
     $user=$form->getData();
     $entityManager->persist($user);
     $entityManager->flush();
-    
-        
     }
     return $this->render('register/index.html.twig', [
         'registerForm' => $form->createView(),
